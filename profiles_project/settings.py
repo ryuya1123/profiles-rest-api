06535ecdfb2bd3d2a0fdd29ae621e01b020afe9e
ryuya1123 <ryuya11235@gmail.com>
2021-01-22 18:45:51 +0900

    Added POST, PUT, PATCH and DELETE methods to our APIView

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'profiles_api',
 ]
 
 MIDDLEWARE = [
