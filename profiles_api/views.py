56004891fefbfce8aeca334421ff84f8891b254f
ryuya1123 <ryuya11235@gmail.com>
2021-01-23 16:40:12 +0900

    Added login API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -84,25 +86,36 @@ class HelloViewSet(viewsets.ViewSet):
 
     def retrieve(self, request, pk=None):
         """Handle getting an object by its ID"""
+        
         return Response({'http_method': 'GET'})
 
     def update(self, request, pk=None):
         """Handle updating an object"""
+        
         return Response({'http_method': 'PUT'})
 
     def partial_update(self, request, pk=None):
         """Handle updating part of an object"""
+        
         return Response({'http_method': 'PATCH'})
     
     def destroy(self, request, pk=None):
         """Handle removing an object"""
+        
         return Response({'http_method': 'DELETE'})
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle createing and updating profiles"""
+    
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication takens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
